e32a3ab50fac6b57cbd4732d479d7005bf619ab5
Vitaliy Sulimenko <prevetal@gmail.com>
2022-05-17 13:33:42 +0300

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,245 @@
+$(() => {
+	// Есть ли поддержка тач событий или это apple устройство
+	if (!is_touch_device() || !/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)) $('html').addClass('custom_scroll')
+
+
+	// Ленивая загрузка
+	setTimeout(() => {
+		observer = lozad('.lozad', {
+			rootMargin: '200px 0px',
+			threshold: 0,
+			loaded: (el) => el.classList.add('loaded')
+		})
+
+		observer.observe()
+	}, 200)
+
+
+	// Установка ширины стандартного скроллбара
+	$(':root').css('--scroll_width', widthScroll() + 'px')
+
+
+	// Маска ввода
+	$('input[type=tel]').inputmask('+7 (999) 999-99-99')
+
+	// Кастомный select
+	$('select:not(.multiple)').niceSelect()
+
+	$('select.multiple').each(function () {
+		$(this).select2({
+			placeholder: $(this).data('placeholder'),
+			dropdownParent: $(this).closest('.field')
+		})
+	})
+
+
+	// Табы
+	var locationHash = window.location.hash
+
+	$('body').on('click', '.tabs button', function (e) {
+		e.preventDefault()
+
+		if (!$(this).hasClass('active')) {
+			const $parent = $(this).closest('.tabs_container'),
+				activeTab = $(this).data('content'),
+				$activeTabContent = $(activeTab),
+				level = $(this).data('level')
+
+			$parent.find('.tabs:first button').removeClass('active')
+			$parent.find('.tab_content.' + level).removeClass('active')
+
+			$(this).addClass('active')
+			$activeTabContent.addClass('active')
+		}
+	})
+
+	if (locationHash && $('.tabs_container').length) {
+		const $activeTab = $('.tabs button[data-content=' + locationHash + ']'),
+			$activeTabContent = $(locationHash),
+			$parent = $activeTab.closest('.tabs_container'),
+			level = $activeTab.data('level')
+
+		$parent.find('.tabs:first button').removeClass('active')
+		$parent.find('.tab_content.' + level).removeClass('active')
+
+		$activeTab.addClass('active')
+		$activeTabContent.addClass('active')
+
+		$('html, body').stop().animate({ scrollTop: $activeTabContent.offset().top }, 1000)
+	}
+
+
+	// Мини всплывающие окна
+	$('body').on('click', '.mini_modal_btn', function (e) {
+		e.preventDefault()
+
+		const modalId = $(this).data('modal-id'),
+			parent = $(this).closest('.modal_cont')
+
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active')
+			$('.mini_modal').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		} else {
+			$('.mini_modal_btn').removeClass('active')
+			$(this).addClass('active')
+
+			$('.mini_modal').removeClass('active')
+
+			modalId === undefined
+				? parent.find('.mini_modal').addClass('active')
+				: $(modalId).addClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'pointer')
+		}
+	})
+
+	// Закрываем всплывашку при клике за её пределами
+	$(document).click((e) => {
+		if ($(e.target).closest('.modal_cont').length === 0) {
+			$('.mini_modal, .mini_modal_btn').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		}
+	})
+
+	// Закрываем всплывашку при клике на крестик во всплывашке
+	$('body').on('click', '.mini_modal .close_btn', (e) => {
+		e.preventDefault()
+
+		$('.mini_modal, .mini_modal_btn').removeClass('active')
+
+		if (is_touch_device()) $('body').css('cursor', 'default')
+	})
+
+
+	// Аккордион
+	$('body').on('click', '.accordion .accordion_item:not(.visible) .head', function (e) {
+		e.preventDefault()
+
+		const $item = $(this).closest('.accordion_item'),
+			$accordion = $(this).closest('.accordion')
+
+		if ($item.hasClass('active')) {
+			$item.removeClass('active')
+			$accordion.find('.accordion_item:not(.visible) .data').slideUp(300)
+		} else {
+			$accordion.find('.accordion_item').removeClass('active')
+			$accordion.find('.accordion_item:not(.visible) .data').slideUp(300)
+
+			$item.addClass('active').find('.data').slideDown(300)
+		}
+	})
+
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.l10n = {
+		CLOSE: "Закрыть",
+		NEXT: "Следующий",
+		PREV: "Предыдущий",
+		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
+	}
+
+	// Всплывающие окна
+	$('body').on('click', '.modal_btn', function (e) {
+		e.preventDefault()
+
+		Fancybox.close()
+
+		Fancybox.show([{
+			src: $(this).data('content'),
+			type: 'inline'
+		}])
+	})
+
+	$('body').on('click', '.modal .close_btn', function (e) {
+		e.preventDefault()
+
+		Fancybox.close()
+	})
+
+	// Увеличение картинки
+	Fancybox.bind('.fancy_img', {
+		Image: {
+			zoom: false,
+		},
+		Thumbs: {
+			autoStart: false,
+		}
+	})
+
+
+	// Плавная прокрутка к якорю
+	$('.scroll_btn').click(function (e) {
+		e.preventDefault()
+
+		let href = $(this).data('anchor'),
+			addOffset = 0
+
+		if ($(this).data('offset')) addOffset = $(this).data('offset')
+
+		$('html, body').stop().animate({ scrollTop: $(href).offset().top - addOffset }, 1000)
+	})
+
+
+	// Моб. версия
+	fiestResize = false
+
+	if ($(window).width() < 375) {
+		$('meta[name=viewport]').attr('content', 'width=375, user-scalable=no')
+
+		fiestResize = true
+	}
+})
+
+
+
+$(window).resize(() => {
+	// Моб. версия
+	if (!fiestResize) {
+		$('meta[name=viewport]').attr('content', 'width=device-width, initial-scale=1, maximum-scale=1')
+		if ($(window).width() < 375) $('meta[name=viewport]').attr('content', 'width=375, user-scalable=no')
+
+		fiestResize = true
+	} else {
+		fiestResize = false
+	}
+})
+
+
+
+// Вспомогательные функции
+const setHeight = (className) => {
+	let maxheight = 0
+
+	className.each(function () {
+		const elHeight = $(this).outerHeight()
+
+		if (elHeight > maxheight) maxheight = elHeight
+	})
+
+	className.outerHeight(maxheight)
+}
+
+
+const is_touch_device = () => !!('ontouchstart' in window)
+
+
+const widthScroll = () => {
+	let div = document.createElement('div')
+
+	div.style.overflowY = 'scroll'
+	div.style.width = '50px'
+	div.style.height = '50px'
+	div.style.visibility = 'hidden'
+
+	document.body.appendChild(div)
+
+	let scrollWidth = div.offsetWidth - div.clientWidth
+	document.body.removeChild(div)
+
+	return scrollWidth
+}
---
+++ js/scripts.js
@@ -0,0 +1,467 @@
+$(() => {
+	// Основной слайдер на главной
+	if ($('.main_slider .swiper-container').length) {
+		new Swiper('.main_slider .swiper-container', {
+			loop: true,
+			speed: 750,
+			watchSlidesVisibility: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 0,
+			slidesPerView: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			}
+		})
+	}
+
+
+	// Премиум обьявление
+	if ($('.ads .ad.premium').length) {
+		var sliders = []
+
+		$('.ads .ad.premium').each(function (index) {
+			$(this).find('.images .big .swiper-container').addClass('s' + index)
+			$(this).find('.images .thumbs button').data('slider-index', index)
+
+			const slider = new Swiper('.s' + index, {
+				loop: false,
+				speed: 500,
+				watchSlidesVisibility: true,
+				slideActiveClass: 'active',
+				slideVisibleClass: 'visible',
+				spaceBetween: 8,
+				slidesPerView: 1,
+				on: {
+					slideChange: swiper => {
+						setTimeout(() => {
+							$(this).find('.images .thumbs button').removeClass('active')
+							$(this).find('.images .thumbs button').eq(swiper.activeIndex).addClass('active')
+						})
+					}
+				}
+			})
+
+			sliders.push(slider)
+		})
+
+		$('.ads .ad.premium .images .thumbs button').click(function (e) {
+			e.preventDefault()
+
+			let sliderIndex = $(this).data('slider-index')
+
+			sliders[sliderIndex].slideTo(parseInt($(this).data('slide-index')), 500)
+		})
+	}
+
+
+	// Товары
+	if ($('.products .swiper-container').length) {
+		let sliders = []
+
+		$('.products .swiper-container').each(function (i) {
+			let slides = $(this).find('.slide').length,
+				this_ID = $(this).attr('id'),
+				options = {
+					loop: false,
+					speed: 500,
+					watchSlidesVisibility: true,
+					slideActiveClass: 'active',
+					slideVisibleClass: 'visible',
+					navigation: {
+						nextEl: '.swiper-button-next',
+						prevEl: '.swiper-button-prev'
+					},
+					breakpoints: {
+						0: {
+							spaceBetween: 16,
+							slidesPerView: 'auto'
+						},
+						480: {
+							spaceBetween: 24,
+							slidesPerView: 'auto'
+						},
+						1024: {
+							spaceBetween: 20,
+							slidesPerView: 3
+						}
+					},
+					on: {
+						init: swiper => {
+							setTimeout(() => {
+								productHeight($(swiper.$el), $(swiper.$el).find('.product').length)
+							})
+						},
+						resize: swiper => {
+							setTimeout(() => {
+								productHeight($(swiper.$el), $(swiper.$el).find('.product').length)
+							})
+						}
+					}
+				}
+
+			sliders[i] = new Swiper('#' + this_ID, options)
+
+			if (slides > sliders[i].params.slidesPerView) {
+				options.loop = true
+				sliders[i].destroy(true, true)
+				sliders[i] = new Swiper('#' + this_ID, options)
+			}
+		})
+	}
+
+
+	// Боковая колонка - фильтр
+	$('aside .mob_btns .filter_btn').click(function (e) {
+		e.preventDefault()
+
+		$(this).toggleClass('active')
+
+		$(this).hasClass('active')
+			? $('aside .filter').slideDown(200)
+			: $('aside .filter').slideUp(200)
+	})
+
+
+	$('.filter .title').click(function (e) {
+		e.preventDefault()
+
+		!$(this).hasClass('active')
+			? $(this).toggleClass('active').next().slideDown(300)
+			: $(this).toggleClass('active').next().slideUp(200)
+	})
+
+
+	$('aside .filter .spoler_btn').click(function (e) {
+		e.preventDefault()
+
+		let parent = $(this).closest('.item')
+
+		$(this).toggleClass('active')
+		parent.find('.hide').slideToggle(300)
+	})
+
+
+	// Страница товара
+	if ($('.product_info .images').length) {
+		const productThumbs = new Swiper('.product_info .thumbs.swiper-container', {
+			loop: false,
+			watchSlidesVisibility: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			slidesPerView: 4,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			breakpoints: {
+				0: {
+					spaceBetween: 12,
+					direction: 'horizontal'
+				},
+				768: {
+					spaceBetween: 16,
+					direction: 'vertical'
+				}
+			}
+		})
+
+		const productSlider = new Swiper('.product_info .big .swiper-container', {
+			loop: false,
+			speed: 500,
+			watchSlidesVisibility: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 20,
+			slidesPerView: 1,
+			thumbs: {
+				swiper: productThumbs
+			}
+		})
+	}
+
+
+	// Обьявление в/из избранно(е/го)
+	$('.favorite_btn').click(function (e) {
+		e.preventDefault()
+
+		$(this).toggleClass('active')
+	})
+
+
+	// Обьявление скрыть/показать
+	$('.ad > .views .btn').click(function (e) {
+		e.preventDefault()
+
+		$(this).toggleClass('active')
+		$(this).closest('.ad').toggleClass('hidden')
+	})
+
+
+	// ЛК - Обьявление - Заявка - Спойлер в описании
+	$('.ad_info .orders .order .description .spoler_btn').click(function (e) {
+		e.preventDefault()
+
+		$(this).closest('.description').find('.text').toggleClass('full')
+		$(this).toggleClass('active')
+	})
+
+
+	// ЛК - Обьявление - Заявка - Смена статуса
+	$('.ad_info .orders .order .status select').change(function () {
+		setTimeout(() => {
+			let parent = $(this).closest('.order'),
+				statusVal = $(this).val()
+
+			if (statusVal == 1) {
+				parent.removeClass('rejected')
+				parent.find('.status .exp').hide()
+				parent.find('.status .exp1').fadeIn(300)
+			}
+
+			if (statusVal == 2) {
+				parent.addClass('rejected')
+				parent.find('.status .exp').hide()
+			}
+
+			if (statusVal == 3) {
+				parent.addClass('rejected')
+				parent.find('.status .exp').hide()
+				parent.find('.status .exp3').fadeIn(300)
+			}
+		})
+	})
+
+
+	// ЛК - Добавление обьявления
+	$('.ad_add .form .section .item .title.spoler_btn').click(function (e) {
+		e.preventDefault()
+
+		$(this).toggleClass('active')
+		$(this).next('.data').slideToggle(300)
+	})
+
+
+	$('.ad_add .form .add_field_btn').click(function (e) {
+		e.preventDefault()
+
+		let template = $(this).next('.template').html()
+
+		$(this).before(template)
+		$(this).prev('.field').find('select').niceSelect()
+	})
+
+
+	$('body').on('click', '.ad_add .form .delete_btn', function (e) {
+		e.preventDefault()
+
+		let field = $(this).closest('.field')
+
+		field.remove()
+	})
+
+
+	$('body').on('click', '.form .files .file .remove_btn', function (e) {
+		e.preventDefault()
+
+		let files = $(this).closest('.files'),
+			file = $(this).closest('.file')
+
+		file.remove()
+
+
+		if (files.find('.file').length == 0) files.remove()
+	})
+
+
+	$('.ad_add .form .checkboxs label').click(function () {
+		let template = $(this).next('.template').html()
+
+		$(this).closest('.checkboxs').next().slideToggle(300)
+	})
+
+
+	// Календарь
+	moment.locale('ru', {
+		months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+	})
+
+	$('input.date_input').daterangepicker({
+		opens: 'left',
+		locale: {
+			format: 'DD MMMM YYYY',
+			applyLabel: 'Готово',
+			cancelLabel: 'Сбросить',
+			fromLabel: 'С',
+			toLabel: 'По',
+			customRangeLabel: 'Custom',
+			daysOfWeek: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+			monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+			firstDay: 1
+		}
+	}, function (start, end, label) {
+		console.log(start)
+	})
+
+
+	// Залипание элемента при прокрутке
+	$('.sticky').stick_in_parent()
+
+
+	// Поле пароля в форме
+	$('.form .view_btn').click(function (e) {
+		e.preventDefault()
+
+		let parent = $(this).closest('.field')
+
+		$(this).toggleClass('active')
+
+		$(this).hasClass('active')
+			? parent.find('.input').attr('type', 'text')
+			: parent.find('.input').attr('type', 'password')
+	})
+
+
+	// Форма - чекбоксы
+	$('.form .checks > .field label').click(function () {
+		let _self = $(this),
+			parent = $(this).closest('.field')
+
+		setTimeout(() => {
+			_self.prev().prop('checked')
+				? parent.find('.sub_checks input[type=checkbox]').prop('checked', true)
+				: parent.find('.sub_checks input[type=checkbox]').prop('checked', false)
+		})
+	})
+
+	$('.form .sub_checks label').click(function () {
+		let _self = $(this),
+			subСhecks = $(this).closest('.sub_checks'),
+			parent = subСhecks.closest('.field')
+
+		setTimeout(() => {
+			subСhecks.find('input[type=checkbox]:checked').length
+				? parent.find('> input[type=checkbox]').prop('checked', true)
+				: parent.find('> input[type=checkbox]').prop('checked', false)
+		})
+	})
+
+
+	// Вакансии - Вакансия
+	$('.vacancies .vacancy .info > .btns .btn.respond_btn, .vacancy_accordion .respond_btn:not(.modal_btn)').click(function (e) {
+		e.preventDefault()
+
+		$(this).toggleClass('active')
+	})
+
+
+	// Страница вакансии
+	$('.vacancy_info .contacts .show_btn').click(function (e) {
+		e.preventDefault()
+
+		$(this).remove()
+		$('.vacancy_info .contacts .info').fadeIn(300)
+	})
+
+
+	// Резюме
+	$('.resumies .resume .info > .btns .invite_btn').click(function (e) {
+		e.preventDefault()
+
+		$(this).toggleClass('active')
+	})
+
+
+	// Моб. каталог
+	$('.catalog .mob_spoler_btn').click(function (e) {
+		e.preventDefault()
+
+		$(this).toggleClass('active')
+
+		$(this).hasClass('active')
+			? $('.content .catalog .category.mob_hide').fadeIn(200)
+			: $('.content .catalog .category.mob_hide').fadeOut(200)
+	})
+
+
+	// Моб. меню
+	$('.mob_header .mob_menu_btn').click((e) => {
+		e.preventDefault()
+
+		$('body').toggleClass('menu_open')
+		$('.mob_header .mob_menu_btn').toggleClass('active')
+		$('header').toggleClass('show')
+
+		$('body').hasClass('menu_open')
+			? $('.overlay').fadeIn(200)
+			: $('.overlay').fadeOut(200)
+	})
+})
+
+
+
+$(window).on('load', () => {
+	// Фикс. шапка
+	moHeaderInit = true,
+		mobHeaderHeight = $('.mob_header').outerHeight()
+
+	$('.mob_header').wrap('<div class="mob_header_wrap"></div>')
+	$('.mob_header_wrap').height(mobHeaderHeight)
+
+	moHeaderInit && $(window).scrollTop() > 0
+		? $('.mob_header').addClass('fixed')
+		: $('.mob_header').removeClass('fixed')
+})
+
+
+
+$(window).resize(() => {
+	// Фикс. шапка
+	moHeaderInit = false
+	$('.mob_header_wrap').height('auto')
+
+	setTimeout(() => {
+		moHeaderInit = true
+		mobHeaderHeight = $('.mob_header').outerHeight()
+
+		$('.mob_header_wrap').height(mobHeaderHeight)
+
+		moHeaderInit && $(window).scrollTop() > 0
+			? $('.mob_header').addClass('fixed')
+			: $('.mob_header').removeClass('fixed')
+	}, 100)
+})
+
+
+
+$(window).scroll(() => {
+	// Фикс. шапка
+	typeof moHeaderInit !== 'undefined' && moHeaderInit && $(window).scrollTop() > 0
+		? $('.mob_header').addClass('fixed')
+		: $('.mob_header').removeClass('fixed')
+})
+
+
+
+// Выравнивание товаров
+function productHeight(context, step) {
+	let start = 0,
+		finish = step,
+		$products = context.find('.product')
+
+	$products.find('.name').height('auto')
+
+	$products.each(function () {
+		setHeight($products.slice(start, finish).find('.name'))
+
+		start = start + step
+		finish = finish + step
+	})
+}
